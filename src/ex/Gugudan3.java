db0704c72746aa81e1844943187a2cbcf4b3752f
YerinYoo <ooyo.che1s@gmail.com>
2024-02-15 17:28:26 +0900

    24.02.15 Java 구구단 예제

---
+++ src/ex/Gugudan3.java
@@ -0,0 +1,28 @@
+package ex;
+
+public class Gugudan3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=2; i<10; i++) {
+			int z =0;
+			
+			for (int j=1;  j<10; j++) {
+				System.out.println(i + " * " + j + " = " +i*j);
+				z += i*j;
+			}
+			System.out.println("----------------------");
+			System.out.println(i + "단합: " + z );
+			System.out.println("----------------------");
+	
+			
+
+
+		}
+
+	}
+
+}
+
+
